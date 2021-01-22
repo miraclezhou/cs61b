9d690db1c75b1c842fe0ad9673c2f0aa53b293d5
miraclezhou <1005113420@qq.com>
2021-01-23 00:19:00 +0800

    lab1 completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author mirazhou
  */
 public class LeapYear {
 
@@ -12,6 +12,20 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    /** Test whether or not a given year is a Leap Year, return
+    	true if it is leap year.
+     *  @param  year to be tested
+     */
+    public static boolean isLeapYear(int year){
+    	if (year % 400 == 0){
+    		return true;
+    	}else if(year % 4 == 0 && year % 100 != 0){
+    		return true;
+    	}else{
+    		return false;
+    	}
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
@@ -30,4 +44,5 @@ public class LeapYear {
         }
     }
 }
+
 
