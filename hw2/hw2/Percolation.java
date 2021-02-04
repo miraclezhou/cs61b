827c5ca7992a5def61498f44957304e59dff6416
miraclezhou <1005113420@qq.com>
2021-02-04 22:22:08 +0800

    hw2 1st

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,127 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private WeightedQuickUnionUF connectedSites;
+
+    // Another wf to check if the site is full, avoid backwash.
+    private WeightedQuickUnionUF checkFull;
+
+    private int rows;
+    private boolean[] openSites;
+    private int openNums;
+    private int top;
+    private int bottom;
+
+    /** create N-by-N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be positive!");
+        }
+        connectedSites = new WeightedQuickUnionUF(N * N + 2);
+        checkFull = new WeightedQuickUnionUF(N * N + 1);
+        openSites = new boolean[N * N];
+        rows = N;
+        openNums = 0;
+        top = rows * rows;
+        bottom = rows * rows + 1;
+    }
+
+    /** open the site (row, col) if it is not open already. */
+    public void open(int row, int col) {
+        int index = xyToIndex(row, col);
+
+        if (!openSites[index]) {
+            openSites[index] = true;
+        }
+        // Connect to the virtual top.
+        if (row == 0) {
+            connectedSites.union(index, top);
+            checkFull.union(index, top);
+        }
+        // Connect to the virtual bottom.
+        if (row == rows - 1) {
+            connectedSites.union(index, bottom);
+        }
+        connectNeighbors(row, col);
+        openSites[xyToIndex(row, col)] = true;
+        openNums += 1;
+    }
+
+    /** Connect the neighbor sites if they are open. */
+    private void connectNeighbors(int row, int col) {
+        // Check left site.
+        if (col > 0) {
+            if (isOpen(row, col - 1)) {
+                connectedSites.union(xyToIndex(row, col), xyToIndex(row, col - 1));
+                checkFull.union(xyToIndex(row, col), xyToIndex(row, col - 1));
+            }
+        }
+        // Check right site.
+        if (col < rows - 1) {
+            if (isOpen(row, col + 1)) {
+                connectedSites.union(xyToIndex(row, col), xyToIndex(row, col + 1));
+                checkFull.union(xyToIndex(row, col), xyToIndex(row, col + 1));
+            }
+        }
+        // Check upper site.
+        if (row > 0) {
+            if (isOpen(row - 1, col)) {
+                connectedSites.union(xyToIndex(row, col), xyToIndex(row - 1, col));
+                checkFull.union(xyToIndex(row, col), xyToIndex(row - 1, col));
+            }
+        }
+        // Check under site.
+        if (row < rows - 1) {
+            if (isOpen(row + 1, col)) {
+                connectedSites.union(xyToIndex(row, col), xyToIndex(row + 1, col));
+                checkFull.union(xyToIndex(row, col), xyToIndex(row + 1, col));
+            }
+        }
+    }
+
+    /** is the site (row, col) open? */
+    public boolean isOpen(int row, int col) {
+        return openSites[xyToIndex(row, col)];
+    }
+
+    /** is the site (row, col) full? */
+    public boolean isFull(int row, int col) {
+        // Check if this site is open.
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        int index = xyToIndex(row, col);
+        // Check if this site is connected to virtual top.
+        return checkFull.connected(index, top);
+    }
+
+    /** Get number of open sites. */
+    public int numberOfOpenSites() {
+        return openNums;
+    }
+
+    /**  does the system percolate? */
+    public boolean percolates() {
+        return connectedSites.connected(top, bottom);
+    }
+
+    private int xyToIndex(int row, int col) {
+        return rows * row + col;
+    }
+
+    /** use for unit testing (not required). */
+    public static void main(String[] args) {
+        Percolation p = new Percolation(5);
+        p.open(0, 1);
+        p.open(1, 1);
+        p.open(1, 2);
+        p.open(2, 2);
+        p.open(2, 3);
+        System.out.println(p.isFull(2, 4));
+        System.out.println(p.percolates());
+        System.out.println(p.numberOfOpenSites());
+
+        System.out.println(p.xyToIndex(2, 3));
+    }
+
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,66 @@
 package hw2;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
+
+    private int times;
+    private int num;
+    private double[] fraction;
+    private Percolation perco;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        // Check argument N and T.
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N or T must be positive!");
+        }
+        num = N;
+        times = T;
+        fraction = new double[times];
+        perco = pf.make(num);
+        doExperiments();
+    }
+
+    private void doExperiments() {
+        for (int i = 0; i < times; i++) {
+            while (!perco.percolates()) {
+                int rowIndex = StdRandom.uniform(5);
+                int colIndex = StdRandom.uniform(5);
+                perco.open(rowIndex, colIndex);
+            }
+            fraction[i] = (double) perco.numberOfOpenSites() / (num * num);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        double sumMean = 0.0;
+        for (int i = 0; i < times; i++) {
+            sumMean += fraction[i];
+        }
+        return sumMean / times;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        if (times == 1) {
+            return Double.NaN;
+        }
+        double sumStddev = 0.0;
+        for (int i = 0; i < times; i++) {
+            sumStddev += (fraction[i] - mean()) * (fraction[i] - mean());
+        }
+        return Math.sqrt(sumStddev / (times - 1));
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(times);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(times);
+    }
 
 }
